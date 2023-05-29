b39c2c46ca7c386824fe68c61e0ed17c70f59f61
chae-yoon <munimooo9765@gmail.com>
2023-05-29 19:49:15 +0900

    Add carousel component

--- src/components/views/PlaceDetailPage/PlaceDetailPage.js
+++ src/components/views/PlaceDetailPage/PlaceDetailPage.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { Carousel } from 'antd'
 
 
 function PlaceDetailPage() {
+  const [imageList, setimageList] = useState([])
   const { placeId } = useParams()
   axios.get(`/api/places/${placeId}`)
   .then(response => {
       console.log(response.data)
+      setimageList(response.data.imageList)
   }).catch(err => {
     console.log(err)
   })
 
   return (
-    <div>PlaceDetailPage</div>
+    <>
+      <Carousel autoplay>
+        {imageList.map(image => <div key={image.id} className='carousel-wrapper'><img src={image.image} alt='place images' className='place-image' /></div>)}
+      </Carousel>
+    </>
+
   )
 }
 
